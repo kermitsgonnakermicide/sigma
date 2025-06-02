18d1ea89dc7df7a07988a2351e364ce7ae247099
kermitsgonnakermicide <daksh.vohra1@gmail.com>
2025-06-02 08:52:53 +0530

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,165 +1,59 @@
-/*
-Author  : Andrea Lombardo
-Site    : https://www.lombardoandrea.com
-Source  : https://github.com/AndreaLombardo/L298N/
+#include "Arduino.h"
+#include <Wire.h>
+ 
+const int MPU_addr=0x68;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+ 
+int minVal=265;
+int maxVal=402;
 
-Here you can see how to work in a common configuration. 
+int ENA = 19;
+int IN1 = 23;
+int IN2 = 18;
 
-Speed range go from 0 to 255, default is 100.
-Use setSpeed(speed) to change.
+double x;
+double y;
+double z;
+ 
+double speed;
 
-Sometimes at lower speed motors seems not running.
-It's normal, may depends by motor and power supply.
-
-Wiring schema in file "L298N - Schema_with_EN_pin.png"
-*/
-
-// Include the library
-/* #include <L298N.h>
-
-// Pin definition
-const unsigned int IN1 = 23;
-const unsigned int IN2 = 22;
-const unsigned int EN = 19;
-String jeff = "";
-int speed = 70;
-// Create one motor instance
-L298N motor(EN, IN1, IN2);
-
-void setup()
-{
-  // Used to display information
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
   Serial.begin(115200);
-  // Set initial speed
-  motor.setSpeed(speed);
 }
-void printSomeInfo()
-{
-  Serial.print("Motor is moving = ");
-  Serial.print(motor.isMoving());
-  Serial.print(" at speed = ");
-  Serial.println(motor.getSpeed());
-}
-void loop()
-{
-  char f = '\n';
-  if (Serial.available()){
-    jeff = Serial.readStringUntil('\n');
-    if (jeff == "a"){
-      speed += 10;
-      motor.setSpeed(speed);
-      Serial.println("Speed: " + speed);
-    }
-    else if (jeff == "b"){
-      speed += -10;
-      motor.setSpeed(speed);
-    }
-    Serial.flush();
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);
+  AcX=Wire.read()<<8|Wire.read();
+  AcY=Wire.read()<<8|Wire.read();
+  AcZ=Wire.read()<<8|Wire.read();
+  int xAng = map(AcX,minVal,maxVal,-90,90);
+  int yAng = map(AcY,minVal,maxVal,-90,90);
+  int zAng = map(AcZ,minVal,maxVal,-90,90);
+
+  x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+  y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+  z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+
+  speed = map(z, 0, 360, 0, 255);
+  Serial.println(speed);
+
+  if (speed > 180){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, speed);
   }
-
-}
-
-/*
-Print some informations in Serial Monitor
-*/
-
-/*
-Author  : Andrea Lombardo
-Site    : https://www.lombardoandrea.com
-Source  : https://github.com/AndreaLombardo/L298N/
-
-Here you can see how to work in a common configuration. 
-
-Speed range go from 0 to 255, default is 100.
-Use setSpeed(speed) to change.
-
-Sometimes at lower speed motors seems not running.
-It's normal, may depends by motor and power supply.
-
-Wiring schema in file "L298N - Schema_with_EN_pin.png"
-*/
-
-// Include the library
-#include <L298N.h>
-
-// Pin definition
-const unsigned int IN1 = 23;
-const unsigned int IN2 = 22;
-const unsigned int EN = 19;
-
-// Create one motor instance
-L298N motor(EN, IN1, IN2);
-
-void setup()
-{
-  // Used to display information
-  Serial.begin(9600);
-
-  // Wait for Serial Monitor to be opened
-  while (!Serial)
-  {
-    //do nothing
+  else{
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN1, LOW);
+    analogWrite(ENA, speed);
   }
-
-  // Set initial speed
-  motor.setSpeed(70);
 }
-
-void printSomeInfo()
-{
-  Serial.print("Motor is moving = ");
-  Serial.print(motor.isMoving());
-  Serial.print(" at speed = ");
-  Serial.println(motor.getSpeed());
-}
-
-void loop()
-{
-
-  // Tell the motor to go forward (may depend by your wiring)
-  motor.forward();
-
-  // Alternative method:
-  // motor.run(L298N::FORWARD);
-
-  //print the motor status in the serial monitor
-  printSomeInfo();
-
-  delay(3000);
-
-  // Stop
-  motor.stop();
-
-  // Alternative method:
-  // motor.run(L298N::STOP);
-
-  printSomeInfo();
-
-  // Change speed
-  motor.setSpeed(255);
-
-  delay(3000);
-
-  // Tell the motor to go back (may depend by your wiring)
-  motor.backward();
-
-  // Alternative method:
-  // motor.run(L298N::BACKWARD);
-
-  printSomeInfo();
-
-  motor.setSpeed(120);
-
-  delay(3000);
-
-  // Stop
-  motor.stop();
-
-  printSomeInfo();
-
-  delay(3000);
-}
-
-/*
-Print some informations in Serial Monitor
-*/
